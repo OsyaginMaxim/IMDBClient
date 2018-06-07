f2fc5dee79e0f6345b14f9f1f79f5187f1a6a23b
Maksim Osyagin <osyaginmaksim@gmail.com>
2018-06-07 18:41:21 +0300

    create tableView and cell

--- IMDBClient/ViewController.m
+++ IMDBClient/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -25,5 +26,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UITableView DataSource Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellId = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    
+    return cell;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 247;
+}
 
 @end
---
+++ Pods/Target Support Files/Pods-IMDBClientTests/Pods-IMDBClientTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IMDBClientTests : NSObject
+@end
+@implementation PodsDummy_Pods_IMDBClientTests
+@end
